635d2b62b7939029cd5b3b32046fe76a2638411e
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-18 03:57:11 -0600

    brought HabitList into Home component

--- HabitStack-react/src/routes/home/home.component.jsx
+++ HabitStack-react/src/routes/home/home.component.jsx
@@ -1,11 +1,29 @@
-import HabitList from "../../components/habit-list/habit-list.component";
 import TimeLine from "../../components/timeline/timeline.component";
 import { Fragment } from "react";
+
+const habits = [
+  { id: 1, name: "Morning Routine" },
+  { id: 2, name: "Animals" },
+  { id: 3, name: "Workout" },
+  { id: 4, name: "After Work" },
+  { id: 5, name: "Wind Down" },
+];
+
+const dragStartHandler = (event) => {
+  // Add the target element's id to the data transfer object
+  event.dataTransfer.setData("text/plain", event.target.id);
+};
 
 const Home = () => {
   return (
     <Fragment>
-      <HabitList></HabitList>
+      <div className="habit-list-container">
+        {habits.map((habit) => (
+          <span draggable="true" key={habit.id} onDragStart={dragStartHandler}>
+            {habit.name}
+          </span>
+        ))}
+      </div>
       <TimeLine></TimeLine>
     </Fragment>
   );
