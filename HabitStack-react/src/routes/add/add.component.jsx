0e80747b7b5b004463cf16d9091d7e3f589b76c7
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-18 07:14:27 -0600

    Added addRoute

--- HabitStack-react/src/routes/add/add.component.jsx
+++ HabitStack-react/src/routes/add/add.component.jsx
@@ -1,5 +1,34 @@
+import { useState } from "react";
+
+import "./add.styles.scss";
+
 const AddRoute = () => {
-  return <div className="add-container">i am add route</div>;
+  const [numberOfHabits, setNumberOfHabits] = useState(0);
+
+  const handleClick = () => {
+    setNumberOfHabits(numberOfHabits + 1);
+  };
+  return (
+    <div className="add-container">
+      <form>
+        <div>
+          <label>Stack Name</label>
+          <input type="text" />
+        </div>
+        <button type="button" onClick={handleClick}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="40px"
+            viewBox="0 -960 960 960"
+            width="40px"
+            fill="#5f6368"
+          >
+            <path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z" />
+          </svg>
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default AddRoute;
