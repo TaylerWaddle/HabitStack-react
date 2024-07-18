bae16631fb2d4c3ebda92a2f5ff2879048de2f89
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-18 02:56:44 -0600

    implemented BrowserRouter

--- HabitStack-react/src/App.js
+++ HabitStack-react/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Navigation from "./routes/navigation/navigation.component";
 import TimeLine from "./components/timeline/timeline.component";
 import HabitList from "./components/habit-list/habit-list.component";
 
