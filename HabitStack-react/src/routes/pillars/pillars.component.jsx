ebbfdc306e04646b344396d230829460f31a4915
Tayler Waddle <taylerwaddle@gmail.com>
2024-07-18 10:58:20 -0600

    Pillars Route Styling

--- HabitStack-react/src/routes/pillars/pillars.component.jsx
+++ HabitStack-react/src/routes/pillars/pillars.component.jsx
@@ -1,5 +1,60 @@
 const PillarsRoute = () => {
-  return <div>I am Pillars Route</div>;
+  return (
+    <div className="route-container">
+      <div className="pillars-graphic-container"></div>
+      <div className="stats-container">
+        <h1 className="stats-header"></h1>
+        <div className="stat">
+          <div className="stat-title">Health</div>
+          <div className="stat-level">lvl 6</div>
+          <div className="xp-bar">
+            ---------------------------------------------------------
+          </div>
+          <div className="progression">400 / 600</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Relationships</div>
+          <div className="stat-level">lvl 6</div>
+          <div className="xp-bar">
+            ---------------------------------------------------------
+          </div>
+          <div className="progression">400 / 600</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Wealth</div>
+          <div className="stat-level">lvl 6</div>
+          <div className="xp-bar">
+            ---------------------------------------------------------
+          </div>
+          <div className="progression">400 / 600</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Romance</div>
+          <div className="stat-level">lvl 6</div>
+          <div className="xp-bar">
+            ---------------------------------------------------------
+          </div>
+          <div className="progression">400 / 600</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Spirituality</div>
+          <div className="stat-level">lvl 6</div>
+          <div className="xp-bar">
+            ---------------------------------------------------------
+          </div>
+          <div className="progression">400 / 600</div>
+        </div>
+        <div className="stat">
+          <div className="stat-title">Career</div>
+          <div className="stat-level">lvl 6</div>
+          <div className="xp-bar">
+            ---------------------------------------------------------
+          </div>
+          <div className="progression">400 / 600</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PillarsRoute;
